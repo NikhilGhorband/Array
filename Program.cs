cc01d994a6db0b96d5e2ed5d9e3516abac6b56bc
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-06-16 23:10:03 +0530

    Encapsulation

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace UC4_ENCAPSULATION
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Oops Concept\n");
+            BankAccount obj = new BankAccount(5000);
+            obj.Deposit(2000);
+
+            Console.WriteLine("After Depositing Money New Balance is " + obj.GetBalance() + "\n");
+
+            obj.Withdraw(500);
+            Console.WriteLine("After Money withdrawal Current Balance is: " + obj.GetBalance());
+            Console.Read();
+        }
+    }
+}
