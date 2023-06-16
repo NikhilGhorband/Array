c5a0d1af27e66da15ee2eaa6a479fc7147f24d3d
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-06-16 23:06:38 +0530

    Abstraction

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace UC4_Abstraction
+{
+    abstract class Animal
+    {
+        public abstract void AnimalSound();
+        public void Eat()
+        {
+            Console.WriteLine("Animal eats ");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void AnimalSound()
+        {
+            Console.WriteLine("Dog Barks ");
+
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Dog dog = new Dog();
+                dog.AnimalSound();
+                dog.Eat();
+
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
