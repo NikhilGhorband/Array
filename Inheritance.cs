5f6d658293321e74ffa4570e9589b921196fd67e
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-06-16 23:16:14 +0530

    Inheritance

---
+++ Inheritance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC2_Inheritance
+{
+    public class Inheritance
+    {
+        public string brand = "LandRover";
+        public void Vehicle()
+        {
+            Console.WriteLine(" Revv");
+        }
+    }
+    class Car : Inheritance
+    {
+        public string modelName = "Velar";
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace UC2_Inheritance
+{
+    internal class Vehicle
+    {
+        public void DisplayVehicle()
+        {
+            Console.WriteLine("LandRover");
+        }
+    }
+
+   
+    
+    // singal Inheritance
+    class Bike : Vehicle
+    {
+        public void DisplayBike()
+        {
+            Console.WriteLine("Discovery");
+        }
+    }
+    // Multilevel Inheritance
+    class Car2 : Car
+    {
+        public void DisplayCar2()
+        {
+            Console.WriteLine("Defender");
+        }
+    }
+    
+}
+
+
+
+
+
+
