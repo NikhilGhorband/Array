f5060392c9266855055bf50878844b80a0ddc649
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-06-16 23:18:43 +0530

    Class And Object

---
+++ ClassAndObjects.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC1_ClassObj
+{
+    internal class ClassAndObjects
+    {
+        public void CreateClassAndObject()
+        {
+            Console.WriteLine("Class and objects Created Succesfully");
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace UC1_ClassObj
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to OOPS Programming");
+                ClassAndObjects objects = new ClassAndObjects();
+                objects.CreateClassAndObject();
+            }
+        }
+    }
